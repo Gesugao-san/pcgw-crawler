4d918b2831049d6a6823fe91541a438ea028d1ae
Gesugao-san <gesugao.san@gmail.com>
2022-02-03 05:55:36 +0300

    Edit edit.js

--- code/edit.js
+++ code/edit.js
@@ -4,6 +4,7 @@
 	MediaWiki API Demos
 	Demo of `Login` module: Sending post request to login
 	https://www.mediawiki.org/wiki/API:Login#JavaScript
+	https://github.com/wikimedia/mediawiki-api-demos/blob/master/javascript/edit.js
 
 	MIT license
 */
@@ -36,21 +37,56 @@ function getLoginToken() {
 // supported. Obtain credentials via Special:BotPasswords
 // (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
 // https://www.pcgamingwiki.com/wiki/Special:BotPasswords
-function loginRequest( loginToken ) {
-	var params = {
-		action: 'login',
-		lgname: 'bot_username',
-		lgpassword: 'bot_password',
-		lgtoken: loginToken,
-		format: 'json'
-	};
+function loginRequest(login_token) {
+    var params_1 = {
+        action: "login",
+        lgname: "bot_username",
+        lgpassword: "bot_password",
+        lgtoken: login_token,
+        format: "json"
+    };
 
-	request.post( { url: url, form: params }, function ( error, res, body ) {
-		if ( error ) {
-			return;
-		}
-		console.log( body );
-	} );
+    request.post({ url: url, form: params_1 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        getCsrfToken();
+    });
+}
+
+// Step 3: GET request to fetch CSRF token
+function getCsrfToken() {
+    var params_2 = {
+        action: "query",
+        meta: "tokens",
+        format: "json"
+    };
+
+    request.get({ url: url, qs: params_2 }, function(error, res, body) {
+        if (error) {
+            return;
+        }
+        var data = JSON.parse(body);
+        editRequest(data.query.tokens.csrftoken);
+    });
+}
+
+// Step 4: POST request to edit a page
+function editRequest(csrf_token) {
+    var params_3 = {
+        action: "edit",
+        title: "Sandbox",
+        appendtext: "test edit",
+        token: csrf_token,
+        format: "json"
+    };
+
+    request.post({ url: url, form: params_3 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        console.log(body);
+    });
 }
 
 // Start From Step 1
