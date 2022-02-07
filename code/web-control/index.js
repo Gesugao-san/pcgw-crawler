be618c6a20b0292945b18bec19b5b79f2b39290b
Gesugao-san <gesugao.san@gmail.com>
2022-02-07 23:36:08 +0300

    Add getJSON

--- code/web-control/index.js
+++ code/web-control/index.js
@@ -15,3 +15,20 @@ var triangleArea = function() {
 
 	alert("Echo:", var1);
 };
+
+// ---
+
+var getJSON = function() {
+	var fs = require('fs');
+	var file = fs.readFileSync('../../config/input.json', 'utf8');
+	var obj = JSON.parse(file);
+	var output2 = document.getElementById('output2');
+
+	if(isNaN(obj.Steam)) {
+		output2.textContent = "Input invalid!";
+		throw new Error("Input invalid!");
+	} else {
+		output2.textContent = "JSON = " + obj.Steam;
+		console.log("2.", obj.Steam);
+	}
+};
