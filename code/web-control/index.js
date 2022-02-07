e3c6354beef1389999962f7b190d36544e8ec769
Gesugao-san <gesugao.san@gmail.com>
2022-02-07 23:29:06 +0300

    Edit to triangleArea

--- code/web-control/index.js
+++ code/web-control/index.js
@@ -1,6 +1,17 @@
 
-var sayEcho = function() {
-	var var1 = toString(document.getElementById('input1').value);
+var triangleArea = function() {
+	var base   = parseFloat(document.getElementById('base'  ).value);
+	var height = parseFloat(document.getElementById('height').value);
+
+	var output = document.getElementById('output');
+
+	if(isNaN(base) || isNaN(height)) {
+		output.textContent = "Input invalid!";
+		throw new Error("Input invalid!");
+	} else {
+		var area = .5 * base * height;
+		output.textContent = "Area = " + area;
+	}
 
 	alert("Echo:", var1);
 };
