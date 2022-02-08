2e436b1deb8a4420db0c3381833c608960e8a055
Gesugao-san <gesugao.san@gmail.com>
2022-02-08 15:07:29 +0300

    Echo func restored and fixed. But for what? IDK.

--- code/web-control/index.js
+++ code/web-control/index.js
@@ -1,19 +1,63 @@
 
-var triangleArea = function() {
-	var base   = parseFloat(document.getElementById('base'  ).value);
-	var height = parseFloat(document.getElementById('height').value);
+var echoSay = function() {
+	var echo_in = document.getElementById('echo_in').value;
+	var echo_status = document.getElementById('echo_status');
+	if(echo_in === "") {
+		/* var error = "Input empty!";
+		alert(echo_in);
+		echo_status.textContent = error;
+		throw new Error(error); */
+		console.debug("Func echoSay() triggered, but input empty, ignoring...");
+		return 0;
+	}
+	var output = "Echo: " + echo_in;
+	console.debug("Func echoSay() triggered, output: \"" + output + "\"");
+	echo_status.textContent = output;
+	alert(output);
+	/* sleep(1);
+	echo_status.textContent = "Waiting for input..."; */
+};
 
-	var output = document.getElementById('output');
+var echoClear = function() {
+	var echo_in = document.getElementById('echo_in');
+	var echo_status = document.getElementById('echo_status');
+	console.debug("Func echoClear() triggered.");
+	echo_in.value = "";
+	echo_status.textContent = "Waiting for input...";
+};
+
+// ---
+
+var triangleСalculate = function() {
+	var base   = parseFloat(document.getElementById('triangle_in1').value);
+	var height = parseFloat(document.getElementById('triangle_in2').value);
+	var output = document.getElementById('triangle_out');
+	var status = document.getElementById('triangle_status');
 
 	if(isNaN(base) || isNaN(height)) {
-		output.textContent = "Input invalid!";
+		var error = "Input invalid!";
+		console.error("Func triggered, but error occured: " + error);
+		status.textContent = error;
+		output.textContent = "";
 		throw new Error("Input invalid!");
 	} else {
 		var area = .5 * base * height;
-		output.textContent = "Area = " + area;
+		output.textContent = area;
+		status.textContent = "Сalculating done.";
 	}
+};
 
-	alert("Echo:", var1);
+var triangleClear = function() {
+	var in1    = document.getElementById('triangle_in1');
+	var in2    = document.getElementById('triangle_in2');
+	var out    = document.getElementById('triangle_out');
+	var status = document.getElementById('triangle_status');
+
+	console.debug("Func triangleClear() triggered.");
+	in1.value = "";
+	in2.value = "";
+	out.textContent = "N\\A";
+	status.textContent = "Waiting for input...";
 };
 
 // ---
@@ -22,13 +66,13 @@ var getJSON = function() {
 	var fs = require('fs');
 	var file = fs.readFileSync('../../config/input.json', 'utf8');
 	var obj = JSON.parse(file);
-	var output2 = document.getElementById('output2');
+	var json_out = document.getElementById('json_out');
 
 	if(isNaN(obj.Steam)) {
-		output2.textContent = "Input invalid!";
+		json_out.textContent = "Input invalid!";
 		throw new Error("Input invalid!");
 	} else {
-		output2.textContent = "JSON = " + obj.Steam;
+		json_out.textContent = "JSON: " + obj.Steam;
 		console.log("2.", obj.Steam);
 	}
 };
