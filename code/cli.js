5530e5b8db07e98f61f2dc52cc17e203a2dd665a
Gesugao-san <gesugao.san@gmail.com>
2022-02-08 16:41:40 +0300

    Add CLI prototype

---
+++ code/cli.js
@@ -0,0 +1,14 @@
+
+var readline = require('readline');
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
+
+rl.on('line', function(line){
+	console.log(line);
+	if (line == "clear") {
+		console.clear();
+	}
+})
