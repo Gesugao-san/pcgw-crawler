1e40f0f90edc7e2d3d0ced4d9199296192863abe
Gesugao-san <gesugao.san@gmail.com>
2022-02-11 15:26:59 +0300

    Add DetermineBaseTemplate

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 
 // https://stackoverflow.com/a/51162901/8175291
 const request = require('request');
-var appid;
+var appid, base_template;
+
+
+function getArrayOfIDs(origin_array) {
+	var keys = [];
+	for (var k in origin_array) {
+		if (typeof origin_array[k].id != "number") // See "genres" ids, IT'S STRINGS, WHY?!
+			origin_array[k].id = parseInt(origin_array[k].id, 10);
+		keys.push(origin_array[k].id);
+	}
+	return keys
+}
+
+function DetermineBaseTemplate(categories_array, genres_array) {
+	var searchData = {
+		"Multiplayer": {
+			"genres": ["29"],
+			"categories": ["1", "9", "20", "27", "36", "37", "38", "39"]
+		},
+		"Singleplayer": {
+			"categories": ["2"]
+		}
+	};
+	var scores = {
+		"Multiplayer": 0,
+		"Singleplayer": 0
+	};
+	for (let i = 0; i < 8; i++) { // all categories of "Multiplayer" category
+		for (let ii = 0; ii < categories_array.length; ii++)
+			if (categories_array[ii].id == searchData["Multiplayer"]["categories"][i])
+				scores["Multiplayer"] += 1;
+	}
+	if (categories_array.includes(searchData["Singleplayer"]["categories"]))
+		scores["Singleplayer"] += 1;
+	if (genres_array.includes(searchData["Multiplayer"]["genres"]))
+		scores["Multiplayer"] += 1;
+
+	console.log("Scores: " + JSON.stringify(scores));
+	if (scores["Multiplayer"] + scores["Singleplayer"] == 0)
+		return "Unknown"
+	if (scores["Multiplayer"] >= scores["Singleplayer"])
+		return "Multiplayer"
+	else
+		return "Singleplayer"
+}
 
 // wrap a request in an promise
 function downloadPage(url) {
@@ -32,7 +76,7 @@ async function main() {
 			return 1;
 		}
 		console.log("Steam API: Requested app is present. Determining base template...");
-		console.log(html[appid].data.categories);
+		console.log("Verdict: Base template is " + DetermineBaseTemplate(html[appid].data.categories, html[appid].data.genres) + ".");
 	} catch (error) {
 		console.error(error);
 		return 1;
